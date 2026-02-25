9c55c69301dca82d3b9803b28c8df7793f2524a6
Zakaria Fekih Ahmed <zakariafekihahmed@gmail.com>
2026-02-25 14:41:39 +0100

    feat: register Patient model in Django admin

--- patients/admin.py
+++ patients/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Patient
 
-# Register your models here.
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "age", "email", "phone", "created_at")
+    search_fields = ("first_name", "last_name", "email", "phone")
+    list_filter = ("created_at",)
