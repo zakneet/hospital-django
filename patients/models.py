234554dec078c18191c7016b84c8f0cd58d1c9b0
Zakaria Fekih Ahmed <zakariafekihahmed@gmail.com>
2026-02-25 14:12:47 +0100

    feat: add Patient model

--- patients/models.py
+++ patients/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Patient(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
