b6d1b0847f491594f2460de466ed2abef72624d0
Zakaria Fekih Ahmed <zakariafekihahmed@gmail.com>
2026-02-25 14:18:38 +0100

    feat: add auth (login/logout) and dashboard

--- patients/views.py
+++ patients/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from .models import Patient
 
-# Create your views here.
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+    return render(request, 'login.html')
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+
+@login_required
+def dashboard(request):
+    total_patients = Patient.objects.count()
+    context = {'total_patients': total_patients}
+    return render(request, 'dashboard.html', context)
