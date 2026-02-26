93f406befa92ef38617b04169500c7dc9b331160
Zakaria Fekih Ahmed <zakariafekihahmed@gmail.com>
2026-02-26 12:11:01 +0100

    ui: bootstrap form widgets for PatientForm

--- patients/forms.py
+++ patients/forms.py
@@ -5,3 +5,10 @@ class PatientForm(forms.ModelForm):
     class Meta:
         model = Patient
         fields = ["first_name", "last_name", "age", "email", "phone"]
+        widgets = {
+            "first_name": forms.TextInput(attrs={"class": "form-control"}),
+            "last_name": forms.TextInput(attrs={"class": "form-control"}),
+            "age": forms.NumberInput(attrs={"class": "form-control"}),
+            "email": forms.EmailInput(attrs={"class": "form-control"}),
+            "phone": forms.TextInput(attrs={"class": "form-control"}),
+        }
