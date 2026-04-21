0faab8267e031f0c2b5089c3048baca657691204
Zakaria Fekih Ahmed <zakariafekihahmed@gmail.com>
2026-04-21 11:45:18 +0100

    chnager ui

--- config/settings.py
+++ config/settings.py
@@ -74,14 +74,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
- 'default': {
-  'ENGINE': 'django.db.backends.mysql',
-  'NAME': 'hospital_db',
-  'USER': 'root',
-  'PASSWORD': '',
-  'HOST': 'localhost',
-  'PORT': '3306',
- }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
